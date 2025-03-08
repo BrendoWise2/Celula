7fb145a6689089ba0e31e2477ecc1b70d3cfe95c
BrendoWise2 <brendo.oliveira@aluno.unincor.edu.br>
2025-03-08 09:12:25 -0300

    Update 1.0 script.js
    All features

--- script.js
+++ script.js
@@ -7,27 +7,57 @@ let startDate, endDate;
 
 btnSalvar.addEventListener('click', function () {
     salvarEquipamento();
+    btnSalvar.disabled = true;
+    setInterval(disabledBnt, 4000);
+
 });
+
+function disabledBnt() {
+    btnSalvar.disabled = false;
+}
 
 
 function salvarEquipamento() {
     operatorName = document.getElementById('operator').value;
-    celulaSelecionada = document.getElementById('celulas');
+    celulaSelecionada = document.getElementById('celulas').value;
 
-    startHour = document.getElementById('startHour');
-    endHour = document.getElementById('endHour');
+    startHour = document.getElementById('startHour').value;
+    endHour = document.getElementById('endHour').value;
 
-    startDate = document.getElementById('startDate');
-    endDate = document.getElementById('endDate');
+    startDate = document.getElementById('startDate').value;
+    endDate = document.getElementById('endDate').value;
 
-    totalTime = document.getElementById('totalTime');
+    totalTime = document.getElementById('totalTime').value;
 
 
-    console.log(celulaSelecionada.value);
-    console.log(operatorName);
-    console.log(startHour.value);
-    console.log(startDate.value);
-    console.log(totalTime.value);
+
+    fetch("http://localhost:8080/celula/salvar", {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            operator: operatorName,
+            celula: celulaSelecionada,
+            startH: startHour,
+            endH: endHour,
+            totalTimeH: totalTime,
+            startD: startDate,
+            endD: endDate,
+
+        })
+
+
+
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Dados enviados com sucesso:", data);
+        })
+        .catch(error => {
+            console.error("Erro ao enviar os dados:", error);
+        });
 
 }
 
